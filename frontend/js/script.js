0a7a998b1084e566798f0b42ff1d79a77ae7fae5
joao <jmazerojorge@gmail.com>
2024-06-12 13:30:13 -0300

    change server url

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -83,7 +83,7 @@ const handleLogin = (event) => {
     login.style.display = "none"
     chat.style.display = "flex"
 
-    websocket = new WebSocket("ws://localhost:8080")
+    websocket = new WebSocket("wss://chat-teste-5u2k.onrender.com")
     websocket.onmessage = processMessage
 }
 
